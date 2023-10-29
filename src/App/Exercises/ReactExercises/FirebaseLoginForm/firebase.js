c2633430276434aad2e0212b91e08ff26cdc79f5
Magda Gadomska <m.srutkowska@gmail.com>
2023-10-29 08:29:12 +0100

    poprawka firebase

---
+++ src/App/Exercises/ReactExercises/FirebaseLoginForm/firebase.js
@@ -0,0 +1,6 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {};
+// dodanie indywidualnego klucza z firebase dodawać i usuwać kazdorazowo
+export const app = initializeApp(firebaseConfig);
